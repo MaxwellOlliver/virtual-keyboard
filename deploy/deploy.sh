eaae5fd9f4000197cfdafdc1f5a7bccea0545919
Maxwell Olliver <maxwellmacedo2015@gmail.com>
2022-06-15 13:16:38 -0300

    Deploy updates

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,11 +8,11 @@ build_command() {
 echo -e "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory
 
-echo -e "\033[0;32mChecking out $branch....\033[0m"
-git worktree add $directory $branch
-
 echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
+
+echo -e "\033[0;32mChecking out $branch....\033[0m"
+git worktree add $directory $branch
 
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
 cd $directory &&
