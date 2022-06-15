9b1d81ec99b298863ece2bffcfde60be10e46522
Maxwell Olliver <maxwellmacedo2015@gmail.com>
2022-06-15 13:24:32 -0300

    modify deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,14 +5,21 @@ build_command() {
   yarn build
 }
 
+# setup() {
+#   git checkout --orphan gh-pages
+#   git reset --hard
+#   git commit --allow-empty -m "Init"
+#   git checkout master
+# }
+
 echo -e "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory
 
-echo -e "\033[0;32mGenerating site...\033[0m"
-build_command
-
 echo -e "\033[0;32mChecking out $branch....\033[0m"
 git worktree add $directory $branch
+
+echo -e "\033[0;32mGenerating site...\033[0m"
+build_command
 
 echo -e "\033[0;32mDeploying $branch branch...\033[0m"
 cd $directory &&
@@ -21,4 +28,4 @@ cd $directory &&
   git push origin $branch
 
 echo -e "\033[0;32mCleaning up...\033[0m"
-git worktree remove $directory
+git worktree prune
